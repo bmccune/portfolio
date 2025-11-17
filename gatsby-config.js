c2c35f7234ac199f1ce88a428938c64b3e1bf0e6
Brian McCune <brian.mccune@gmail.com>
2025-11-17 13:54:50 -0800

    Fix build issues: Add missing SEO component and create icon for manifest
    - Created src/components/seo.js to resolve import error
    - Added src/images/icon.svg for gatsby-plugin-manifest
    - Fixed build process by providing required icon file
    - Build now completes successfully

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
         background_color: `#1a202c`,
         theme_color: `#2d3748`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.svg`, // Simple BM initials icon
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
